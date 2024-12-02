a5355c255d4f104a3072d2beb8de027c8c4575bf
Bismit Panda <adnaptimsib333@gmail.com>
2024-12-02 18:17:16 +0530

    day 2

---
+++ src/bin/day2-part1.rs
@@ -0,0 +1,23 @@
+use itertools::Itertools;
+
+fn main() {
+    let safe = include_str!(r"../../input/day2.txt")
+        .trim()
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect_vec()
+        })
+        .filter(|values| {
+            values
+                .windows(2)
+                .all(|w| w[0] > w[1] && (w[0] - w[1] <= 3) && (w[0] - w[1] >= 1))
+                || values
+                    .windows(2)
+                    .all(|w| w[0] < w[1] && (w[1] - w[0] <= 3) && (w[1] - w[0] >= 1))
+        })
+        .count();
+
+    dbg!(safe);
+}
